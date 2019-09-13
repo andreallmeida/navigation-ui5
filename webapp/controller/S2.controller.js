937973339922054f89a0c1702b5756b7ca62ea29
André Almeida <andre.allmeida@gmail.com>
2019-09-13 22:04:16 +0000

    Try

---
+++ webapp/controller/S2.controller.js
@@ -0,0 +1,66 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/routing/History"
+], function (Controller,History) {
+	"use strict";
+
+	return Controller.extend("ovly.navigation-ui5.controller.S2", {
+
+		/**
+		 * Called when a controller is instantiated and its View controls (if available) are already created.
+		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+		 * @memberOf ovly.navigation-ui5.view.S2
+		 */
+		onInit: function () {
+
+		},
+
+		onNavButtonPress: function () {
+
+			// Identifica Histórico de chamadas
+			var oHistory = History.getInstance();
+			
+			// Busca página anterior, caso exista
+			var sPreviousHash = oHistory.getPreviousHash();
+
+			// Se existir pagina anterior, volta 1 na pilha de chamadas
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+				
+			// Senao, retorna para uma "rota" default
+			} else {
+				var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+				oRouter.navTo("");
+			}
+
+		}
+
+		/**
+		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+		 * (NOT before the first rendering! onInit() is used for that one!).
+		 * @memberOf ovly.navigation-ui5.view.S2
+		 */
+		//	onBeforeRendering: function() {
+		//
+		//	},
+
+		/**
+		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+		 * This hook is the same one that SAPUI5 controls get after being rendered.
+		 * @memberOf ovly.navigation-ui5.view.S2
+		 */
+		//	onAfterRendering: function() {
+		//
+		//	},
+
+		/**
+		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+		 * @memberOf ovly.navigation-ui5.view.S2
+		 */
+		//	onExit: function() {
+		//
+		//	}
+
+	});
+
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"ovly/navigation-ui5/test/unit/controller/S0.controller"
+], function () {
+	"use strict";
+});
